f57167563e570f17e8af383f7db5e0bf6b0fc91e
michaeldars99-jpg <michaeldars99@gmail.com>
2026-01-17 14:55:43 +0100

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -13,18 +13,27 @@ const page = await context.newPage();
 
 
 
-await page.goto(url, {
+await page.goto('https://makerworld.com/en/@Davson_Art', {
   waitUntil: 'domcontentloaded',
   timeout: 60000
 });
 
-await page.waitForTimeout(3000);
-const stats = await page.evaluate(async () => {
-  const res = await fetch(
-    'https://makerworld.com/api/v1/user-service/user/profile?username=Davson_Art'
-  );
-  return await res.json();
-});
+await page.waitForTimeout(5000);
+const response = await page.request.get(
+  'https://makerworld.com/api/v1/user-service/user/profile?username=Davson_Art',
+  {
+    headers: {
+      'accept': 'application/json',
+      'user-agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36'
+    }
+  }
+);
+
+const stats = await response.json();
+console.log('RAW STATS:', JSON.stringify(stats, null, 2));
+
+
 
 console.log('RAW STATS:', JSON.stringify(stats, null, 2));
 
@@ -39,6 +48,7 @@ console.log(content.slice(0, 2000));
 
 
 await browser.close();
+
 
 
 
